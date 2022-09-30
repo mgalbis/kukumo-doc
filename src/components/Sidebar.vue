f1996bff63ddcddcfdd624fea27b80267ba6ad58
mgalbis <mgalbis@iti.es>
2022-09-30 10:33:30 +0200

    added multilanguage

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -25,7 +25,7 @@
 
 <static-query>
 query Menu {
-  menu: allMenu(order:ASC, filter: { locale: { eq: $i18n.locale } }) {
+  menu: allMenu(order:ASC, filter: { locale: { eq: $locale } }) {
     edges {
       node {
         section
