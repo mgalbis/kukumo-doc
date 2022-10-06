5f8c8077aef8656db1648448de0fdfc8c26b65a2
mgalbis <mgalbis@iti.es>
2022-10-06 16:33:23 +0200

    added multilanguage

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ require('typeface-source-sans-pro')
 export default function (Vue, { router, head, isClient, appOptions }) {
   Vue.use(Vuex)
 
-  console.log("Main options: " + Object.keys(appOptions))
+  console.log("Main options: " + JSON.stringify(appOptions.data))
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
