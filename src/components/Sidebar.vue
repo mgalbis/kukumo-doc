ea19a51beaf4f99604b2173d22995dccd579a0ed
mgalbis <mgalbis@iti.es>
2022-10-06 17:30:02 +0200

    added multilanguage

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -80,10 +80,7 @@ export default {
       return edges.filter( e => e.node.locale == this.$context.locale)
     },
     checkAnchors(node, item) {
-      console.log("Node: " + JSON.stringify(node))
-      console.log("Item: " + JSON.stringify(item))
-      console.log("--------------------------------")
-      if (node.slug == item.slug) {
+      if (node.slug == `/${item.slug}`) {
         if (item.headings.length > 0) {
           node.headings = item.headings
         }
