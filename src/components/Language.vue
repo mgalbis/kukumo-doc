cdcd2feb8a6f728bbd0c1ec11ecaa361813740ed
mgalbis <mgalbis@iti.es>
2022-10-12 12:20:49 +0200

    added multilanguage

--- src/components/Language.vue
+++ src/components/Language.vue
@@ -1,10 +1,10 @@
 <template>
   <button id="languageSwitch" @click="toggleLanguage()" aria-label="Switch language between spanish and english">
     <transition name="theme">
-      <globe-icon v-if="$context.locale == 'es'" class="globe" /> EN
+      <globe-icon v-if="$context.locale == 'es'" class="globe" /> {{ 'EN' }}
     </transition>
     <transition name="theme">
-      <globe-icon v-if="$context.locale == 'en'" class="globe" /> ES
+      <globe-icon v-if="$context.locale == 'en'" class="globe" /> {{ 'ES' }}
     </transition>
   </button>
 </template>
